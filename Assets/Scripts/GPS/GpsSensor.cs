a29098fd6163abc77f2b635a98119824e329af39
calm0815(kuwano) <kuwano.tomohiro455@mail.kyutech.jp>
2020-05-11 15:22:54 +0900

    add GPS files

---
+++ Assets/Scripts/GPS/GpsSensor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class GpsSensor : UnityPublisher<MessageTypes.Sensor.NavSatFix>
+    {
+        public string FrameId = "gps";
+        private MessageTypes.Sensor.NavSatFix message;
+
+        protected override void Start()
+        {
+            base.Start();
+            InitializeMessage();
+        }
+
+        private void FixedUpdate()
+        {
+            UpdateMessage();
+        }
+
+        private void InitializeMessage()
+        {
+            message = new MessageTypes.Sensor.NavSatFix();
+            message.header.frame_id = FrameId;
+        }
+
+        private void UpdateMessage()
+        {
+            message.header.Update();
+            GetLatitude();
+            GetLongitude();
+            GetAltitude();
+            Publish(message);
+        }
+
+        private void GetLatitude()
+        {
+
+        }
+
+        private void GetLongitude()
+        {
+
+        }
+
+        private void GetAltitude()
+        {
+
+        }
+    }
+}
